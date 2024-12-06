937f8c50e9d6e4fedd3111f5795a36edf30ef19c
Chris Smith <chris@chameth.com>
2024-12-06 12:55:10 +0000

    Day 6: more performance improvements
    Maintain a separate set of co-ords and co-ords+directions
    instead of generating the former from the latter

--- 06.hs
+++ 06.hs
@@ -34,24 +34,23 @@ partOne :: [Position] -> Int
 partOne = length . S.fromList . map fst
 
 partTwo :: Grid -> [Position] -> Int
-partTwo grid path = length $ S.fromList $ scan S.empty grid path
+partTwo grid path = length $ S.fromList $ scan S.empty S.empty grid path
     where
-    scan :: S.Set Position -> Grid -> [Position] -> [Coord]
-    scan _ _ [] = []
-    scan _ _ [_] = []
-    scan visited grid (pos:next:others)
+    scan :: S.Set Coord -> S.Set Position -> Grid -> [Position] -> [Coord]
+    scan _ _ _ [] = []
+    scan _ _ _ [_] = []
+    scan visitedCoords visited grid (pos:next:others)
         -- Just spinning on the spot, don't drop an obstacle on their head
-        | currentCoord == nextCoord = scan visited grid (next:others)
+        | currentCoord == nextCoord = scan visitedCoords visited grid (next:others)
         -- We've already travelled over the next cell, can't block it
-        | S.member nextCoord visitedCoords = scan visited grid (next:others)
+        | S.member nextCoord visitedCoords = scan visitedCoords visited grid (next:others)
         -- Placing an obstacle in front of us causes a loop
-        | loop visited grid nextCoord pos = nextCoord:scan (S.insert next visited) grid (next:others)
+        | loop visited grid nextCoord pos = nextCoord:scan (S.insert nextCoord visitedCoords) (S.insert next visited) grid (next:others)
         -- Obstacle in front doesn't work
-        | otherwise = scan (S.insert next visited) grid (next:others)
+        | otherwise = scan (S.insert nextCoord visitedCoords) (S.insert next visited) grid (next:others)
         where
             currentCoord = fst pos
             nextCoord = fst next
-            visitedCoords = S.map fst visited
 
             loop :: S.Set Position -> Grid -> Coord -> Position -> Bool
             loop visited grid obstacle pos
