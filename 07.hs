c773c97e929233f5345a52debfd63511de3c74b6
Chris Smith <chris@chameth.com>
2024-12-07 12:34:25 +0000

    Day 7, slow edition

---
+++ 07.hs
@@ -0,0 +1,32 @@
+module Main where
+
+parseInput :: String -> (Int, [Int])
+parseInput line = (read (takeWhile (/= ':') target), map read values)
+  where
+    (target : values) = words line
+
+partOne :: [(Int, [Int])] -> Int
+partOne lines = sum $ map (evaluate partOneExpand) lines
+
+partTwo :: [(Int, [Int])] -> Int
+partTwo lines = sum $ map (evaluate partTwoExpand) lines
+
+evaluate :: ([Int] -> [Int] -> [Int]) -> (Int, [Int]) -> Int
+evaluate expand (target, initial : values)
+  | target `elem` expand [initial] values = target
+  | otherwise = 0
+
+partOneExpand :: [Int] -> [Int] -> [Int]
+partOneExpand = foldl (\inputs value -> map (* value) inputs ++ map (+ value) inputs)
+
+partTwoExpand :: [Int] -> [Int] -> [Int]
+partTwoExpand = foldl (\inputs value -> map (* value) inputs ++ map (+ value) inputs ++ map (cc value) inputs)
+
+cc :: Int -> Int -> Int
+cc b a = read ("" ++ show a ++ show b)
+
+main :: IO ()
+main = do
+  input <- map parseInput . lines <$> readFile "inputs/07.txt"
+  print $ partOne input
+  print $ partTwo input
