5503117bce70d57d69f7c46e352b08c74bdde586
Chris Smith <chris@chameth.com>
2024-12-07 13:11:19 +0000

    Day 7: don't check part 2 if part 1 worked

--- 07.hs
+++ 07.hs
@@ -1,20 +1,20 @@
 module Main where
 
+import Common (add)
+
 parseInput :: String -> (Int, [Int])
 parseInput line = (read (takeWhile (/= ':') target), map read values)
   where
     (target : values) = words line
 
-partOne :: [(Int, [Int])] -> Int
-partOne lines = sum $ map (evaluate partOneExpand) lines
+solve :: [(Int, [Int])] -> (Int, Int)
+solve lines = foldl add (0, 0) (map evaluate lines)
 
-partTwo :: [(Int, [Int])] -> Int
-partTwo lines = sum $ map (evaluate partTwoExpand) lines
-
-evaluate :: ([Int] -> [Int] -> [Int]) -> (Int, [Int]) -> Int
-evaluate expand (target, initial : values)
-  | target `elem` expand [initial] values = target
-  | otherwise = 0
+evaluate :: (Int, [Int]) -> (Int, Int)
+evaluate (target, initial : values)
+  | target `elem` partOneExpand [initial] values = (target, target)
+  | target `elem` partTwoExpand [initial] values = (0, target)
+  | otherwise = (0, 0)
 
 partOneExpand :: [Int] -> [Int] -> [Int]
 partOneExpand = foldl (\inputs value -> map (* value) inputs ++ map (+ value) inputs)
@@ -27,11 +27,12 @@ cc b a = a * 10 ^ digitCount b + b
 
 digitCount :: Int -> Int
 digitCount = go 1
-    where
-        go digits target = if target >= 10 then go (digits + 1) (div target 10) else digits
+  where
+    go digits target = if target >= 10 then go (digits + 1) (div target 10) else digits
 
 main :: IO ()
 main = do
   input <- map parseInput . lines <$> readFile "inputs/07.txt"
-  print $ partOne input
-  print $ partTwo input
+  let (partOne, partTwo) = solve input
+  print partOne
+  print partTwo
