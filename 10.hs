fba958cf0978fc00592b8baa6cb5fe8eeed6abe8
Chris Smith <chris@chameth.com>
2024-12-10 22:17:40 +0000

    Day 10: some mild perf improvements

--- 10.hs
+++ 10.hs
@@ -1,7 +1,7 @@
 module Main where
 
-import Data.Array.IArray (assocs, (!))
 import Common (Coord, Grid, add, outOfBounds, parseGrid, unique)
+import Data.Array.IArray (assocs, (!))
 import Data.Char (digitToInt)
 
 cardinals :: [Coord]
@@ -9,16 +9,15 @@ cardinals = [(0, -1), (0, 1), (1, 0), (-1, 0)]
 
 findPaths :: Grid -> [(Coord, [Coord])]
 findPaths grid = map (\x -> (x, trace 0 x)) (starts grid)
-    where
-        starts = map fst . filter (\(_, v) -> v == '0') . assocs
-        trace :: Int -> Coord -> [Coord]
-        trace target coord
-            | value /= target = []
-            | value == 9 = [coord]
-            | otherwise = concatMap (trace (target + 1)) neighbours
-            where
-                value = digitToInt $ grid ! coord
-                neighbours = filter (not . outOfBounds grid) $ map (add coord) cardinals
+  where
+    starts = map fst . filter ((== '0') . snd) . assocs
+    trace :: Int -> Coord -> [Coord]
+    trace target coord
+      | value == 9 = [coord]
+      | otherwise = concatMap (trace (target + 1)) neighbours
+      where
+        value = digitToInt $ grid ! coord
+        neighbours = filter ((== target + 1) . digitToInt . (grid !)) $ filter (not . outOfBounds grid) $ map (add coord) cardinals
 
 partOne :: [(Coord, [Coord])] -> Int
 partOne = sum . map (unique . snd)
@@ -26,8 +25,12 @@ partOne = sum . map (unique . snd)
 partTwo :: [(Coord, [Coord])] -> Int
 partTwo = sum . map (length . snd)
 
+solve :: [(Coord, [Coord])] -> (Int, Int)
+solve = foldl add (0, 0) . map (\(_, x) -> (unique x, length x))
+
 main :: IO ()
 main = do
   input <- findPaths . parseGrid <$> readFile "inputs/10.txt"
-  print $ partOne input
-  print $ partTwo input
+  let (partOne, partTwo) = solve input
+  print partOne
+  print partTwo
